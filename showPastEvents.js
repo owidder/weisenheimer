e8886643ea39d2b64f359e80d65ad5c345acee1e
Oliver Widder <oliver.widder@gmail.com>
2019-09-02 08:13:48 +0200

    refactor + textarea + hashAndLogButton

---
+++ showPastEvents.js
@@ -0,0 +1,15 @@
+window.showPastEvents = (contract, selector) => {
+    contract.getPastEvents("NewHashValue", {fromBlock: 0, toBlock: 'latest'}, function (error, events) {
+        console.log(events);
+        const data = events.map(function (event) {
+            return {
+                blockNumber: event.blockNumber,
+                senderAddress: event.returnValues[1],
+                timestamp: new Date(event.returnValues[2] * 1000).toDateString(),
+                hashValue: event.returnValues[0],
+            }
+        });
+
+        window.showDataAsTable(selector, data.reverse(), "blockNumber", "responsive-table striped");
+    });
+}
